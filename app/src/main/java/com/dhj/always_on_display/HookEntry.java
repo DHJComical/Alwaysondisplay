079813faec1cdb14995291833a7ce494b9020c0d
DHJComical <dhjcomical@163.com>
2026-06-27 12:34:22 +0800

    Keep scoped apps awake

--- app/src/main/java/com/dhj/always_on_display/HookEntry.java
+++ app/src/main/java/com/dhj/always_on_display/HookEntry.java
@@ -1,5 +1,12 @@
 package com.dhj.always_on_display;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.lang.reflect.Method;
+
 import io.github.libxposed.api.XposedModule;
 import io.github.libxposed.api.XposedModuleInterface;
 
@@ -15,6 +22,50 @@ public class HookEntry extends XposedModule {
     @Override
     public void onPackageLoaded(XposedModuleInterface.PackageLoadedParam param) {
         super.onPackageLoaded(param);
-        log(android.util.Log.INFO, TAG, "Target package loaded: " + param.getPackageName());
+        hookActivityLifecycle(param.getPackageName());
+    }
+
+    private void hookActivityLifecycle(String packageName) {
+        try {
+            Method onCreate = Activity.class.getDeclaredMethod("onCreate", Bundle.class);
+            Method onResume = Activity.class.getDeclaredMethod("onResume");
+
+            hook(onCreate)
+                    .setId("always_on_display_activity_on_create")
+                    .intercept(chain -> {
+                        Object result = chain.proceed();
+                        keepScreenOn(chain.getThisObject(), packageName);
+                        return result;
+                    });
+
+            hook(onResume)
+                    .setId("always_on_display_activity_on_resume")
+                    .intercept(chain -> {
+                        Object result = chain.proceed();
+                        keepScreenOn(chain.getThisObject(), packageName);
+                        return result;
+                    });
+
+            log(android.util.Log.INFO, TAG, "Keep-screen-on hooks installed for: " + packageName);
+        } catch (NoSuchMethodException e) {
+            log(android.util.Log.ERROR, TAG, "Unable to find Activity lifecycle methods", e);
+        } catch (Throwable t) {
+            log(android.util.Log.ERROR, TAG, "Unable to install keep-screen-on hooks for: " + packageName, t);
+        }
+    }
+
+    private void keepScreenOn(Object thisObject, String packageName) {
+        if (!(thisObject instanceof Activity)) {
+            return;
+        }
+
+        Activity activity = (Activity) thisObject;
+        Window window = activity.getWindow();
+        if (window == null) {
+            return;
+        }
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        log(android.util.Log.DEBUG, TAG, "Keep screen on: " + packageName + "/" + activity.getClass().getName());
     }
 }
